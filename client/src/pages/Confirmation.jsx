1380f25973f2e15d612e7be0561a7ae1787bb615
amirreza azemati <amirrezaazemati@Mac.fritz.box>
2025-09-24 11:44:07 +0200

    sendMail verification

--- client/src/pages/Confirmation.jsx
+++ client/src/pages/Confirmation.jsx
@@ -28,7 +28,7 @@ function Confirmation() {
     };
 
     try {
-      const res = await fetch('https://cispace.onrender.com/api/reserve', {
+      const res = await fetch('http://localhost:5001/api/reserve', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload),
